bbbada5b9897822fa679e4bb391fc0153235244e
Atomarverseucht <atomcraft.4u@web.de>
2026-06-01 16:27:58 +0200

    lock based tree structure

--- 29-lock-based-data-structures/lock-tree.c
+++ 29-lock-based-data-structures/lock-tree.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Data-structure -> Tree
 typedef struct element_ {
     int value;
     struct element_* child[2];
@@ -12,25 +13,47 @@ typedef struct element_ {
 
 element* head;
 
+void lockElement(element* active){
+    pthread_mutex_lock(active->lock);
+    while(active->used != 0) 
+        pthread_cond_wait(active->condition, active->lock);
+    active->used = 1;
+}
+
+void unlockElement(element* active){
+    active->used = 0;
+    pthread_cond_signal(active->condition);
+    pthread_mutex_unlock(active->lock);
+}
+
 element* newElement(int value){
     element* out = malloc(sizeof(element));
     out->value = value;
     out->used = 0;
-    out->child[0] = malloc(sizeof(element));
-    out->child[1] = malloc(sizeof(element));
+    out->child[0] = NULL;
+    out->child[1] = NULL;
     out->lock = malloc(sizeof(pthread_mutex_t));
     out->condition = malloc(sizeof(pthread_cond_t));
-    if (out->condition == NULL || out->lock == NULL || out->child[0] == NULL || out->child[1] == NULL) {
+    if (out->condition == NULL || out->lock == NULL) {
         perror("Speicherallokation fehlgeschlagen");
-        exit(1);
+        exit(-1);
     }
 
     pthread_mutex_init(out->lock, NULL);
     if (pthread_cond_init(out->condition, NULL) != 0) {
         perror("Cond-Initialisierung fehlgeschlagen");
-        exit(1);
+        exit(-1);
     }
     return out;
+}
+
+int countElements(element* active){
+    int count = 1;
+    lockElement(active);
+    if(active->child[0] != NULL) count += countElements(active->child[0]);
+    if(active->child[1] != NULL) count += countElements(active->child[1]);
+    unlockElement(active);
+    return count;
 }
 
 void freeTree(element* active){
@@ -46,32 +69,48 @@ void addElement(int value){
     element* lastElem = head;
     int isHead = 1;
     while(activeElem != NULL){
-        pthread_mutex_lock(activeElem->lock);
-        if (!isHead) pthread_mutex_unlock(lastElem->lock);
+        lockElement(activeElem);
+        if (!isHead) unlockElement(lastElem);
         isHead = 0;
         lastElem = activeElem;
         activeElem = lastElem->child[(value < lastElem->value)?1:0];
     }
     lastElem->child[(value < lastElem->value)?1:0] = newElement(value);
-    pthread_mutex_unlock(lastElem->lock);
+    unlockElement(lastElem);
 }
 
 void printTree(element* active){
-    printf("%d: %d, %d\n", active->value, active->child[0]->value, active->child[1]->value);
+    printf("%04d: ", active->value);
+    if(active->child[0] != NULL) printf("%04d, ", active->child[0]->value);
+    else printf("----, ");
+    if(active->child[1] != NULL) printf("%04d", active->child[1]->value);
+    else printf("----");
+    printf("\n");
     if(active->child[0] != NULL) printTree(active->child[0]);
     if(active->child[1] != NULL) printTree(active->child[1]);
 }
 
+/// ANWENDUNG
+
 void* worker(void* args){
+    for(int i = 0; i < 10; i++){
+        addElement(rand() % 100000);
+    }
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
+    int threadCount = 1000;
+    pthread_t thread[threadCount];
     head = newElement(11);
-    int values[] = {10,14,9,12,8,27};
-    for(int i = 0; i < 6; i++){
-        addElement(values[i]);
+    for(int i = 0; i < threadCount; i++){
+        pthread_create(&thread[i], NULL, worker, NULL);
     }
+    for(int i = 0; i < threadCount; i++){
+        pthread_join(thread[i], NULL);
+    }
+
     printTree(head);
+    printf("Count: %d\n", countElements(head));
     freeTree(head);
 }
